df991b142cb17c5e5ad72c87b2e479ed4ce076f2
Fabian Holler <fabian.holler@simplesurance.de>
2022-01-25 15:21:30 +0100

    vendor: update bunny-go

--- go.mod
+++ go.mod
@@ -3,12 +3,13 @@ module github.com/simplesurance/bunny-cli
 go 1.17
 
 require (
-	github.com/simplesurance/bunny-go v0.0.0-20211029092120-2410703bde32
+	github.com/simplesurance/bunny-go v0.0.0-20220602151353-a31ca2b1226d
 	github.com/spf13/cobra v1.2.1
 )
 
 require (
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
--- vendor/github.com/simplesurance/bunny-go/pullzone_edgerule_delete.go
+++ vendor/github.com/simplesurance/bunny-go/pullzone_edgerule_delete.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 )
 
-// Delete removes an Edge Rule of a Pull Zone.
+// DeleteEdgeRule removes an Edge Rule of a Pull Zone.
+// The edgeRuleGUID field is called edgeRuleID in the API message and
+// documentation. It is the same then the GUID field in the EdgeRule message.
 //
 // Bunny.net API docs: https://docs.bunny.net/reference/pullzonepublic_deleteedgerule
-func (s *EdgeRuleService) Delete(ctx context.Context, edgeRuleGUID string) error {
-	req, err := s.client.newDeleteRequest(fmt.Sprintf("pullzone/%d/edgerules/%s", s.pullZoneID, edgeRuleGUID), nil)
+func (s *PullZoneService) DeleteEdgeRule(ctx context.Context, pullZoneID int64, edgeRuleGUID string) error {
+	req, err := s.client.newDeleteRequest(fmt.Sprintf("pullzone/%d/edgerules/%s", pullZoneID, edgeRuleGUID), nil)
 	if err != nil {
 		return err
 	}
